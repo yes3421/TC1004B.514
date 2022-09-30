09d4be8da43fabc06c34c582aeb55f3cff8ee45a
alecksandr <66882463+alecksandr26@users.noreply.github.com>
2022-09-30 08:51:51 -0500

    yeah now you have a more secure code here paul
    You need to do write a better code paul a secure code.

--- fork.c
+++ fork.c
@@ -3,20 +3,24 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#include <string.h>
+#include <errno.h>
+
 int main(int argc, char *argv[])
 {
-    pid_t pid = fork();
+    pid_t pid;
 
-    if (pid == 0) {
-        printf("Soy el proceso hijo.\n");
-
+    if ((pid = fork()) < 0) {
+        fprintf(stderr, "Error: main: fork: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+        
+    } else if (pid == 0) {
+        printf("Soy el proceso hijo con pid %u.\n", getpid());
         execl("/workspace/TC1004B.514/hello", "hello", NULL);
-
         sleep(5);
-    }
-    else {
-        printf("Soy el proceso padre.\n");
-
+        
+    } else {
+        printf("Soy el proceso padre con pid %u.\n", getpid());
         wait(NULL);
     }
 
