bada6b37867c3552a42bb2fe56e3d41ca32f917b
yes3421 <karl_heinrich1883@outlook.com>
2022-10-06 13:12:41 +0000

    Terminate signal

--- signal.c
+++ signal.c
@@ -2,18 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 void hello(int signal)
 {
     printf("Received signal %d\n", signal);
 }
 
+int condition = 0;
+
+void terminate(int signal)
+{
+    printf("Terminating while\n");
+
+    condition = 0;
+}
+
 int main(int argc, char *argv[])
 {
     signal(2, hello);
-    signal(12, hello);
+    signal(12, terminate);
 
-    while (1) {
+    condition = 1;
+
+    while (condition) {
         printf("Executing\n");
 
         sleep(1);
