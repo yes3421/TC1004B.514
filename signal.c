011e0abefb6fb503cd899967697e7150dd57e4b8
yes3421 <karl_heinrich1883@outlook.com>
2022-10-06 12:43:51 +0000

    New file

---
+++ signal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void hello(int signal)
+{
+    printf("Received signal %d\n", signal);
+}
+
+int main(int argc, char *argv[])
+{
+    signal(12, hello);
+
+    while (1) {
+        printf("Executing\n");
+
+        sleep(1);
+    }
+
+    printf("This is never executed\n");
+
+    return EXIT_SUCCESS;
+}
