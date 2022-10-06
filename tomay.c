4ed4a6a519f58fa2dd3cf24eacf0c134ba7154d9
yes3421 <karl_heinrich1883@outlook.com>
2022-10-06 13:36:00 +0000

    New file

---
+++ tomay.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    char c;
+    char upper;
+
+    do {
+        n = read(STDIN_FILENO, &c, 1);
+
+        upper = toupper(c);
+
+        write(STDOUT_FILENO, &upper, 1);
+    } while (n != 0);
+
+    return EXIT_SUCCESS;
+}
