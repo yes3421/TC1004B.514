287d2e910a4a8c71d33c61b78cf757f6043e225d
yes3421 <karl_heinrich1883@outlook.com>
2022-10-13 01:32:49 +0000

    Done

--- tomay.c
+++ tomay.c
@@ -9,11 +9,18 @@ int main(int argc, char *argv[])
     int n;
     char c;
     char upper;
+    char vowels[] = {'A', 'E', 'I', 'O', 'U'};
 
     do {
         n = read(STDIN_FILENO, &c, 1);
 
         upper = toupper(c);
+
+        for (int i = 0; i != 5; ++i) {
+            if (upper == vowels[i]) {
+                upper = '1';
+            }
+        }
 
         write(STDOUT_FILENO, &upper, 1);
     } while (n != 0);
