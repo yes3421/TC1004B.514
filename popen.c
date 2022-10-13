c553ec6a630024ee7c6f5a9f7162eb09fa643db6
yes3421 <karl_heinrich1883@outlook.com>
2022-10-13 12:51:20 +0000

    Added lines

--- popen.c
+++ popen.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 80
+
 int main(int argc, char *argv[])
 {
     FILE *output;
     FILE *input;
 
+    char read_buffer[MAX_SIZE];
+
     output = popen("ls", "r");
-    
+
     input = popen("./tomay", "w");
+
+    while (fgets(read_buffer, 80, output)) {
+        fputs(read_buffer, input);
+    }
+
+    pclose(output);
+    
+    pclose(input);
 
     return EXIT_SUCCESS;
 }
